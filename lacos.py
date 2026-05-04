59165d618b9c1fa774ec506cd0da3ada8d4026a2
FabiolaCury <dufabilitwinski@gmail.com>
2026-05-04 19:48:33 -0300

    1alteracao

--- lacos.py
+++ lacos.py
@@ -1,2 +1,16 @@
 print("Início da aula")
 
+#Código de faixas etárias
+
+idade = int(input("Qual sua idade?"))
+
+if idade <= 12 :
+    print("Você é criança.")
+elif idade < 18 :
+    print("Você é adolescente.")
+elif idade < 65 :
+    print("Você é adulto.")
+else:
+    print("Você é idoso.")
+
+
