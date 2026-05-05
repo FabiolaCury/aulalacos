2f9b22d29145b113ceaacfb08fb34b6b6c2d9418
FabiolaCury <dufabilitwinski@gmail.com>
2026-05-04 21:17:36 -0300

    3alteracao

--- lacos.py
+++ lacos.py
@@ -19,6 +19,18 @@ else:
 # print(10 <= 10)
 # print(10 != 5)
 # print(10 != 10)
+# print(10 == 10)
 
+
+idade = int(input("Digite a sua idade:"))
+
+cnh = input("Possui habilitação?(sim/não):")
+
+if idade >= 18 and cnh == "sim":
+    print("Você tem permissão para dirigir.")
+else :
+    print("Você não tem permissão para dirigir.")
+
+    
 
 
