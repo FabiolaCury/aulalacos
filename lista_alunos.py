b1f7da8ab2ef208d468f2cb52a1a31fbbe75e060
FabiolaCury <dufabilitwinski@gmail.com>
2026-05-18 22:09:27 -0300

    Add files via upload

---
+++ lista_alunos.py
@@ -0,0 +1,11 @@
+# alunos = ["Ian", "Ana", "Bruna", "João", "Lucas"]
+#para cada aluno em alunos imprima
+# for aluno in alunos:
+#     print(aluno)
+
+alunos = ["Monique", "Marco", "João", "Eduardo"]
+
+while aluno in alunos:
+    print(aluno)
+    break
+
